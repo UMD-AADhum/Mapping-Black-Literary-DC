8b00120e6ad4fd865da6281d2ad33fb470f3fbaa
lmalexander <42302324+lmalexander@users.noreply.github.com>
2022-10-07 17:31:45 -0400

    fallingText() is working!

--- scripts/mbldc-falling-text.js
+++ scripts/mbldc-falling-text.js
@@ -1,76 +1,27 @@
 console.log("falling-text is connected!")
 
-// >>> chiData 
-let chiData = {
-    "centerName": [
-        "Hostway Boeing Data Center",
-        "Lumen Chicago",
-        "XO Chicago",
-        "CBOT Data Center",
-        "FDCservers.net Chicago",
-        "CoreSite Chicago (CH2)",
-        "Lumen Chicago 3",
-        "ANET Chicago",
-        "Cogent Chicago 2",
-        "QTS Chicago"
-    ],
-    "companyName" : [
-        "Hostway Services, Inc",
-        "Lumen",
-        "XO Communications",
-        "Data Stream LLC",
-        "FDCservers.net LLC",
-        "CoreSite",
-        "ANET Internet Solutions",
-        "Cogent Communications, Inc.",
-        "QTS Data Centers"
-    ]
-};
-
-// create variables   
-let chiDataName = "";
-let chiDataComp = "";
-
-// set fallingText() to run every 1200 milliseconds
-setInterval(getText, 2400);
-
-// >>> getText function
-function getText() {
-
-    // store a random center name from chiData to a variable
-    for (let index = 0; index < chiData.centerName.length; index++) {
-        chiDataName = chiData.centerName[Math.floor(Math.random() * 8)];
-        console.log(chiDataName);
-
-    // store a random company name from chiData to a variable
-        chiDataComp = chiData.companyName[Math.floor(Math.random() * 8)];
-        console.log(chiDataComp);
-    };
-
-    // returns variable contents to browser window as text
-    // document.getElementById("txtChiDataName").innerText = chiDataName;
-    // document.getElementById("txtChiDataComp").innerText = chiDataComp;
-}
-
 let fallTime = null;
 
 // >>> fallingText function
 
 function fallingText() {
 // grab HTML elements to be moved
-    let fallName = document.getElementById("txtChiDataName");
-    let fallComp = document.getElementById("txtChiDataComp");
+    let fallName = document.getElementById("divChiDataName");
+    let fallComp = document.getElementById("divChiDataComp");
 
 // set starting element position
     let position = 0;
 
 // set timing for element movement
-    timing = setInterval(fall, 10);
+    clearInterval(fallTime);
+    fallTime = setInterval(fall, 10);
 
 // move elements according to timing
     function fall() {
         if (position == 500) {
             clearInterval(fallTime);
+            fallTime = setInterval(fall, 10);
+            position = 0
         }   
         else {
             position++;
@@ -78,4 +29,4 @@ function fallingText() {
             fallComp.style.left = position + 'px';
         }
     }
-}
+};
