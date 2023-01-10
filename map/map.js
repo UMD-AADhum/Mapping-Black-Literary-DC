1c6b5923713c247ad3dc3c772bedd10633876554
lmalexander <42302324+lmalexander@users.noreply.github.com>
2023-01-10 17:47:31 -0500

    can return map markers from mapPoints array

--- map/map.js
+++ map/map.js
@@ -1,11 +1,10 @@
 console.log("and it is I, the javascript behind the leaflet map")
 
-
 pageMap = document.getElementById("map");
 
 // ***** mbldc markers array: 0-venue, 1-address, 2-lat, 3-long,
  let mapPoints = [
-    ["Harambee House Hotel", "2225 Georgia Ave, NW, Washington DC 20059", 38.9200080201834, -77.0216320019951 ]
+    ["Harambee House Hotel", "2225 Georgia Ave, NW, Washington DC 20059", 38.9200080201834, -77.0216320019951]
     ]; 
 
 
@@ -14,44 +13,28 @@ pageMap = document.getElementById("map");
 // Howard University coordinates: 38.9227° N, 77.0194° W -- map center
 map = L.map(pageMap).setView({lon: -77.0194, lat: 38.9227}, 12);
 
-
 // add the OpenStreetMap tile
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-}).addTo(map); 
-
-
+}).addTo(map);
 
 // show the scale bar on the lower left corner
 L.control.scale({imperial: true, metric: true}).addTo(map);
 
-
-
-// show a marker on the map. ID numbers correspond to the ID on table rows
-// 1
-L.marker({lon: -77.017208, lat: 38.903657}).bindPopup('440 L ST WASHINGTON DC 20001-2587').addTo(map);
-
-
-
-
-
 // console.log(map);
 // console.log(pageMap);
 
-
-
-
-// ***** add points to leaflet map
+// ***** add points to leaflet map from mapPoints array
 
  for (let index = 0; index < mapPoints.length; index++) {
-    let lon = mapPoints[index][2];
+    let lat = mapPoints[index][2];
+    console.log(lat);
+    
+    let lon = mapPoints[index][3];
     console.log(lon);
 
-    let lat = mapPoints[index][3];
-    console.log(lat);
-
-    let popup = mapPoints[index][0]+" Located at "+mapPoints[index][1];
+    let popup = mapPoints[index][0]+ "<br>" +"Address: "+mapPoints[index][1];
     console.log(popup);
 
     let pointLoc = new L.LatLng(lat, lon);
@@ -62,7 +45,6 @@ L.marker({lon: -77.017208, lat: 38.903657}).bindPopup('440 L ST WASHINGTON DC 20
 
     map.addLayer(pointMarker);
     pointMarker.bindPopup(popup);
-
     
 }
 
