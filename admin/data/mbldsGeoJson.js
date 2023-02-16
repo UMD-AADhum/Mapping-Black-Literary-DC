f6c0be83175d4fed24c8e0ec2014f40be405259d
lmalexander <42302324+lmalexander@users.noreply.github.com>
2023-02-15 19:20:25 -0500

    moved geoJSON inside of showData function

--- admin/data/mbldsGeoJson.js
+++ admin/data/mbldsGeoJson.js
@@ -34,36 +34,40 @@ function showData(result) {
     console.log(pageDataTest);
 
     document.getElementById('papa-parse-connected').innerText = pageDataTest;
+
+    let geoJSON = {
+        type: "FeatureCollection",
+        features: [],
+      };
+      
+      for (i = 0; i < rawData.length; i++) {
+        if (window.CP.shouldStopExecution(1)) {
+          break;
+        }
+        geoJSON.features.push({
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [rawData[i].Longitude, rawData[i].Latitude]
+          },
+          "properties": {
+            "id": rawData[i].UID,
+            "Venue": rawData[i].Venue,
+            "venueType": rawData[i].venueType,
+            "Address": rawData[i].Address,
+            "popupContent": rawData[i].Address
+          }
+        });
+      };
+    
+      console.log(geoJSON);
+      
+      window.CP.exitedLoop(1);
+      
+      document.getElementById('json').innerHTML = JSON.stringify(rawData, null, 2);
+      document.getElementById('geo-json').innerHTML = JSON.stringify(geoJSON, null, 2);
+    
 };
+ 
+console.log(rawData);
 
-let geoJSON = {
-    type: "FeatureCollection",
-    features: [],
-  };
-  
-  for (i = 0; i < rawData.length; i++) {
-    if (window.CP.shouldStopExecution(1)) {
-      break;
-    }
-    geoJSON.features.push({
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [rawData[i].Longitude, rawData[i].Latitude]
-      },
-      "properties": {
-        "id": rawData[i].UID,
-        "Venue": rawData[i].Venue,
-        "venueType": rawData[i].venueType,
-        "Address": rawData[i].Address,
-        "popupContent": rawData[i].Address
-      }
-    });
-  };
-
-  console.log(geoJSON);
-  
-  window.CP.exitedLoop(1);
-  
-  document.getElementById('json').innerHTML = JSON.stringify(rawData, null, 2);
-  document.getElementById('geo-json').innerHTML = JSON.stringify(geoJSON, null, 2);
