cafabc9f01f2fda910c484407348d5eac80900cb
lmalexander <42302324+lmalexander@users.noreply.github.com>
2023-07-07 08:34:36 -0400

    map.js cleanup

--- scripts/map.js
+++ scripts/map.js
@@ -1,55 +1,54 @@
 console.log("map.js is connected")
-// a. script pulls csv data from Google Sheet to json with papa parse
-// b. adds json to mbldsGeoJson.js
 
-// = c. return geojson to map as points on map.js page & shows geojson on geojson-view.html
-
-// A - papa parse data return
-// * this function returns MBLDC CSV data uploaded via Google Sheets share link as JSON 
-
-// Google Sheets URL
+// ********** VARIABLES
+// > Google Sheets URL
 // 1. upload an updated data file to Google Sheets
 // 2. click 'File' > 'Share' > 'Publish to the web'
-// 3. under 'link' tab, click 'webpage' dropdown and select CSV file from the menu
+// 3. under 'Link' tab, click 'Webpage' dropdown and select CSV file from the menu
 // 4. copy & paste link below
-
-let mapCards = document.getElementById("map-cards");
-
-// *1 - grab map div from DOM
-pageMap = document.getElementById("map");
-
-// *2 - create map & set main view center 
-// ^ current center: Logan Circle | latitude = 38.909142, longitude -77.029793
-map = L.map(pageMap).setView({lon: -77.029793, lat: 38.909142}, 14.5);
-
-// *3 - add map tile
-// tile: toner, stamen maps
-L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-}).addTo(map);
-
-// *4 - show scale bar on the lower left corner
-L.control.scale({imperial: true, metric: true}).addTo(map);
-
-
+// 5. verify CSV by scrolling to very end of URL: ending should be '&output=csv'
 let mbldcGSheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTftLVLS-R7Osjh6O60IornfVPoG2MK1TS7HluHkc6DE_uOwdKl75FsZLmPC7pWUcP_XsHiaSYajGmI/pub?gid=397357141&single=true&output=csv ";
 
+// > map cards div
+let mapCards = document.getElementById("map-cards");
+
+// > geoJSON collection
 let geoJSON = {
     type: "FeatureCollection",
     features: [],
 };
 
+// ********** LEAFLET.JS MAP
+// > grab map div from DOM
+pageMap = document.getElementById("map");
+
+// > create map & set main view center 
+// >>> current center: Logan Circle | latitude = 38.909142, longitude -77.029793
+map = L.map(pageMap).setView({lon: -77.029793, lat: 38.909142}, 14.5);
+
+// > add map tiles
+// >>> tile: toner, stamen maps
+L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+}).addTo(map);
+
+// > show scale bar on the lower left corner
+L.control.scale({imperial: true, metric: true}).addTo(map);
+
+// ********** DATA RETURN
+// > papaparse CSV to JSON pull
 Papa.parse(mbldcGSheetURL, {
     download: true,
     header: true,
     complete: showData,
 });
 
+// > MBLDC data return function 
 function showData(result) {
 
     let rawData = result.data;
-      
+// >>> push rawData to geoJSON  
     for (let index = 0; index < rawData.length; index++) {
         geoJSON.features.push({
             "type": "Feature",
@@ -70,57 +69,61 @@ function showData(result) {
                 }
         });
     };
-    console.log(geoJSON);
+    // console.log(geoJSON);
+
+// >>> push geoJSON data to map with popup *FIX*    
     L.geoJSON(geoJSON).addTo(map).bindPopup("content - fix");
 
-// *2 - loop through mapPoints to create and add listing card for each
+// > create map cards
+
+// >>> loop through geoJSON and add card for each
 for (let index = 0; index < geoJSON.features.length; index++) {
    
-    // a. create card column to go in DOM map-cards row
+    // create card column to go in DOM map-cards row
     // let cardCol = document.createElement("div");
     // cardCol.className = "col";
 
-    // b. create card element
+// >>>>> create card element
     let card = document.createElement("div");
     // card.className = "card grid-item";
     card.className = "card";
 
-    // set card ID
+// >>>>> set card ID
     card.setAttribute("id", geoJSON.features[index].properties.id);
     console.log(card.id);
 
-    // c. create card body
+// >>>>> create card body
     let cardBody = document.createElement("div");
     cardBody.className = "card-body";
 
-    // d. create card title & store mapPoints data
+// >>>>> create card title & store geoJSON data
     let cardTitle = document.createElement("h5");
     cardTitle.className = "card-title"
     cardTitle.innerText = geoJSON.features[index].properties.venueName;
 
-    // create card category & store mapPoints data
+// >>>>> create card category & store geoJSON data
     let cardCat = document.createElement("h6")
     cardCat.className = "card-subtitle";
     cardCat.innerText = geoJSON.features[index].properties.venueType;
 
-    // e. create card text & store mapPoints data
+// >>>>> create card text & store geoJSON data
     let cardText = document.createElement("p");
     cardText.className = "card-text"
     cardText.innerText = geoJSON.features[index].properties.address;
 
-    // create card img & store mapPoints img tag
+// >>>>> create card img & store geoJSON img tag
     let cardImg = document.createElement("img");
     cardImg.className = "card-img-top";
     cardImg.setAttribute("src", "./elements/img/archive/thumbnails/" + geoJSON.features[index].properties.imgUID + ".jpg");
 
-    // create card link & store mapPoints external links
+// >>>>> create card link & store geoJSON external links
     let cardExtUrl = document.createElement("a");
     cardExtUrl.className = "btn btn-primary";
     cardExtUrl.setAttribute("href", geoJSON.features[index].properties.extURL);
     cardExtUrl.setAttribute("target", "blank")
     cardExtUrl.innerText = "Visit"
 
-    // f. append card elements to card; 
+// >>>>> append card elements to card; 
     cardBody.appendChild(cardTitle);
     cardBody.appendChild(cardCat);
     cardBody.appendChild(cardText);
@@ -129,13 +132,13 @@ for (let index = 0; index < geoJSON.features.length; index++) {
     card.appendChild(cardBody);
     
 
-    // g. append card to DOM card row div
+// >>>>> append card to DOM card row div
     mapCards.append(card);
 };
 
 };
 
-
+// > map details 
 let wmataOverImg = "./elements/img/maps/wmata-map-495.png";
 let errorOverlayImg = "https://cdn-icons-png.flaticon.com/512/110/110686.png";
 let wmataOverBounds = L.latLngBounds([[39.028492, -77.233734], [38.768111, -76.844407]])
@@ -148,7 +151,6 @@ let imageOverlay = L.imageOverlay(wmataOverImg , wmataOverBounds, {
 
 
 // scrollIntoView buttons
-
 function scrollIntoViewPoint() {
     let scrollPointID = this.properties.id;
     console.log(scrollPointID);
